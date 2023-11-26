a9e35ee3fd374015ab7bad6577b35e960d8579ff
hassane <skikrihassane@gmail.com>
2023-11-26 13:58:32 +0100

    complete the home section

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-let words =document.querySelectorAll(".word"); //select all the elements in the document that match a specified css selector word 
-
-words.forEach((word)=>{ //iterates over each item in the word NodeList
+let words =document.querySelectorAll(".word"); 
+words.forEach((word)=>{
     let letters = word.textContent.split("");
-    word.textContent=""; //remove all the text content from the word element
+    word.textContent=""; 
     letters.forEach((letter)=>{
-        let span = document.createElement("span");//create a span element
-        span.textContent = letter;//after this the span element will contain the same content of the current letter element
+        let span = document.createElement("span");
+        span.textContent = letter;
         span.className = "letter";
         word.append(span);
-        //this allows each caracter to be styled individually and tranditioned separatley .
-
     });
 });
 
@@ -19,14 +16,12 @@ words[WordIndex].style.opacity = "1";
 
 let changingText = ()=>{
     let currentWord = words[WordIndex];
-    //store the next word in the list words
-    let nextWord = WordIndex === maxwordIndex ? words[0] : words[WordIndex +1]//this is a ternary conditional expression , it checks if wordIndex is equal to maxWordIndex if it is it return words[0] that means we've reached the end of the list 
-
-    Array.from(currentWord.children).forEach((letter,i )=>{ //letter for the current letter and i for the index ==>create an array that contain each letter of the word and iterates over eacn letter of the word
-        setTimeout(()=>{ //htis function is used to delay the execution of the specified function  by a specific number of miliseconds
+    let nextWord = WordIndex === maxwordIndex ? words[0] : words[WordIndex +1];
+    Array.from(currentWord.children).forEach((letter,i )=>{ 
+        setTimeout(()=>{ 
             letter.className="letter out";
-        },i*80);  // so this ensures that each letter is hidden with a delay that is proportional to its position in the word
-    }); //the letters of the current word are hidden one by one with a delay
+        },i*80);
+    });
     nextWord.style.opacity = "1";
     Array.from(nextWord.children).forEach((letter,i )=>{ 
         letter.className = "letter behind";
@@ -38,4 +33,4 @@ let changingText = ()=>{
 };
 
 changingText();
-setInterval(changingText,3000); //caling the changing text function every 3 seconds 
+setInterval(changingText,3000);
